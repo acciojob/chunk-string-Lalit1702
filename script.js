f5664ac025c9658a7cab684837b0d1030588a619
Lalit Patil <147392059+Lalit1702@users.noreply.github.com>
2024-08-18 22:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return []; // Return an empty array if the input string is null or empty
+
+    const chunks = [];
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.slice(i, i + size)); // Slice the string into chunks
+    }
+    return chunks;
 }
 
 // Do not change the code below
